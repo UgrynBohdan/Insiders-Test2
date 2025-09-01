8cec8ca43106e8e84200fa267db4e057f9d6ab54
UgrynBohdan <ugrynbogdan7@gmail.com>
2025-09-01 12:33:32 +0300

    feat: add user login functionality

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,18 +1,47 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 import { connectDB } from '../db/db'
 import User from '../db/models/User'
+import { CustomError } from '../middlewares/error'
+import jwt from 'jsonwebtoken'
+
+const SECRET_KEY = process.env.SECRET_KEY as string
 
 export async function register(req: Request, res: Response) {
     const { name, email, password } = req.body
 
     await connectDB()
-    const newUser = new User({ name, email, password })
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const newUser = new User({ name, email, password: hash })
     await newUser.save()
-    res.json(newUser)
+    res.status(201).json(newUser)
 }
 
 export async function login(req: Request, res: Response) {
+    const { email, password } = req.body
+
     await connectDB()
-    const users = await User.find()
-    res.json({ users })
+
+    const row = await User.find({ email })
+    const user = row[0]
+    if (!user) {
+        const err = new Error() as CustomError
+        err.message = 'User is not found!'
+        err.status = 404
+        throw err
+    }
+
+    await bcrypt.compare(password, user.password).catch((e) => {
+        const err = new Error() as CustomError
+        err.message = 'Incorrect password!'
+        err.status = 401
+        throw err
+    })
+    
+    const token = jwt.sign({ id: user._id, name: user.name, role: user.role }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 60 * 60 * 1000 })
+    res.status(200).json({ message: 'Login senseful!' });
 }
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,6 +4,6 @@ import * as authController from '../controllers/auth.controller'
 const authRouter = Router()
 
 authRouter.post('/register', authController.register)
-authRouter.get('/login', authController.login)
+authRouter.post('/login', authController.login)
 
 export default authRouter
